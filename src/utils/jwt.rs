c5ac0561614c15d3620173143434e43cd473c5dd
Sxmmie <samuelumohjnr@gmail.com>
2026-06-10 21:30:22 +0100

    Added token verification

--- src/utils/jwt.rs
+++ src/utils/jwt.rs
@@ -1,3 +1,9 @@
+use axum::http::StatusCode;
+use axum_extra::{
+    TypedHeader,
+    headers::{Authorization, authorization::Bearer},
+};
+use jsonwebtoken::{DecodingKey, Validation, decode};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -5,4 +11,15 @@ pub struct Claims {
     pub id: i32,
     pub sub: String,
     pub exp: usize,
+}
+
+pub async fn verify_auth_token(TypedHeader(auth): TypedHeader<Authorization<Bearer>>) -> Result<Claims, StatusCode> {
+    // extra raw token in a string
+    let token = auth.token();
+
+    // load secrets from env
+    let secret = std::env::var("JWT_SECRET").unwrap_or_else(|_| "my_secret".into());
+
+    // attempt to decode and validate token
+    let token_data = decode(token, &DecodingKey::from_secret(secret.as_bytes), &Validation::default()).map_err(|_| StatusCode::UNAUTHORIZED)
 }
